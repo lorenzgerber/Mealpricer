2659aa4e3aa3eb5af5d9057f9dabef4605d58208
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-08-03 11:58:57 +0200

    started with some activities

--- app/src/main/java/com/loge/mealpricer/MealPricerTabActivity.java
+++ app/src/main/java/com/loge/mealpricer/MealPricerTabActivity.java
@@ -1,5 +1,6 @@
 package com.loge.mealpricer;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -60,8 +61,10 @@ public class MealPricerTabActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MealPricerTabActivity.this, ProductActivity.class);
+                startActivity(intent);
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
             }
         });
 
