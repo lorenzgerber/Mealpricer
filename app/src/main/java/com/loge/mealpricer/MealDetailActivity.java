07c1e39c03519e21960ab4223943b2e3750961f3
Lorenz Gerber <lorenz.gerber@lablicate.com>
2017-08-15 10:53:21 +0200

    in code start of mealdetail frag

--- app/src/main/java/com/loge/mealpricer/MealDetailActivity.java
+++ app/src/main/java/com/loge/mealpricer/MealDetailActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -45,6 +47,15 @@ public class MealDetailActivity extends AppCompatActivity
             }
         });
 
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.ingredient_fragment_container);
+
+        if (fragment == null ){
+            fragment = new IngredientListFragment();
+            fm.beginTransaction()
+                    .add(R.id.ingredient_fragment_container, fragment)
+                    .commit();
+        }
 
     }
 
